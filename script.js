56ef9f5202b3979f442760d3e3bfa5e61e1060a9
naomiboschman <115638225+naomiboschman@users.noreply.github.com>
2023-05-24 14:03:11 +0000

    veel gedaan

--- script.js
+++ script.js
@@ -1,119 +1,170 @@
-"use strict"
-//Vijand=SnoopDogg
-//Speler=Jointje
-
+"use strict";
+// Vijand = SnoopDogg
+// Speler = Jointje
 
 const SPELEN = 1;
 const GAMEOVER = 2;
 var spelStatus = SPELEN;
 
-//Speler aangemaakt
-var spelerX = 430; 
-var spelerY = 200; 
+// Speler aangemaakt
+var spelerX = 430;
+var spelerY = 200;
 
-//Vijand aangemaakt
+// Vijand aangemaakt
 var vijandX = [800, 1200, 1600, 2000, 2400];
 var vijandY = [600, 400, 200, 100, 300];
 
-//beweegsnelheid van Vijand
-var vijandSnelheid = 3; 
+// Munten aangemaakt
+var muntX = [1000, 1400, 1800, 2200, 2600];
+var muntY = [500, 300, 100, 200, 400];
+var muntGepakt = [false, false, false, false, false];
 
-//Score 
+// Beweegsnelheid van Vijand en munten
+var vijandSnelheid = 3;
+var muntSnelheid = 2;
+
+// Score, highscore en aantal opgepakte munten
 var score = 0;
 var highScore = 0;
+var muntenGepakt = 0;
 
-//Niet dubbeldrukken
+// Tijd en timer interval
+var tijd = 0;
+var timerInterval = 1000; // 1 seconde
+
+// Niet dubbeldrukken
 var toetsnu = false;
 var toetsnet = false;
 
-//Decoratie voor Vijand+Speler+achtergrond
+// Decoratie voor Vijand+Speler+achtergrond
 var img;
 var karakter;
 var SpelerImg;
+var muntImg;
 
-//Random cijfer maken 
+// Random cijfer maken
 function krijgWillekeurigCijfer(max) {
   return Math.floor(Math.random() * max);
 }
 
-//Laat het game bewegen
+// Laat het game bewegen
 var beweegAlles = function() {
-  toetsnet = toetsnu; // zorgen dat er niet ingedrukt kan worden
+  toetsnet = toetsnu; // Zorgen dat er niet ingedrukt kan worden
   toetsnu = keyIsDown(32); // Speler een richting geven
   if (toetsnet === false && toetsnu === true) {
-    spelerY = spelerY - 70; // standaard naar beneden 
+    spelerY = spelerY - 70; // Standaard naar beneden
   } else {
-    spelerY = spelerY + 4; //1x omhoog met Spacebar
+    spelerY = spelerY + 4; // 1x omhoog met Spacebar
   }
 
-  //als soeler buiten het speelvlak gaat is gameover
+  console.log("SpelerY:", spelerY);
+  // Als speler buiten het speelvlak gaat is gameover
   if (spelerY < 50 || spelerY > 720) {
     spelStatus = GAMEOVER;
   }
-
 
   for (var t = 0; t < vijandY.length; t = t + 1) {
     vijandX[t] = vijandX[t] - vijandSnelheid;
     if (vijandX[t] < -100) {
       score++;
-      vijandX[t] = random(1000, 1500); 
-      vijandY[t] = random(100, 400); 
+      vijandX[t] = random(1000, 1500);
+      vijandY[t] = random(100, 400);
     }
+  }
+
+  for (var i = 0; i < muntY.length; i++) {
+    muntX[i] = muntX[i] - muntSnelheid;
+    if (muntX[i] < -100) {
+      muntX[i] = random(1000, 1500);
+      muntY[i] = random(100, 400);
+      muntGepakt[i] = false;
+    }
+  }
+
+  tijd += deltaTime; // Tijd bijhouden
+  if (tijd >= timerInterval) {
+    tijd = 0; // Reset de tijd
+    vijandSnelheid += 0.5; // Verhoog de vijandsnelheid na elke 10 seconden
   }
 };
 
+// Verwerk botsing met vijand en munten
 var verwerkBotsing = function() {
- 
   for (var t = 0; t < vijandY.length; t = t + 1) {
     if (
-      spelerX < vijandX[t] + 80 && 
-      spelerX + 50 > vijandX[t] && 
-      spelerY < vijandY[t] + 200 && 
-      spelerY + 50 > vijandY[t] 
+      spelerX < vijandX[t] + 80 &&
+      spelerX + 50 > vijandX[t] &&
+      spelerY < vijandY[t] + 200 &&
+      spelerY + 50 > vijandY[t]
     ) {
-      spelStatus = GAMEOVER; 
-      break; 
+      spelStatus = GAMEOVER;
+      break;
+    }
+  }
+
+  for (var i = 0; i < muntY.length; i++) {
+    if (
+      spelerX < muntX[i] + 50 &&
+      spelerX + 50 > muntX[i] &&
+      spelerY < muntY[i] + 50 &&
+      spelerY + 50 > muntY[i] &&
+      !muntGepakt[i]
+    ) {
+      muntGepakt[i] = true;
+      score++;
+      muntenGepakt++;
     }
   }
 };
 
-
-
+// Teken alle elementen van het spel
 var tekenAlles = function() {
-
   image(img, 0, 0, 1500, 750);
 
-  fill('black')
+  fill("black");
 
   for (var t = 0; t < vijandY.length; t = t + 1) {
-    image(SpelerImg, vijandX[t], vijandY[t], 80, 200); 
+    image(SpelerImg, vijandX[t], vijandY[t], 80, 200);
+  }
+
+  for (var i = 0; i < muntY.length; i++) {
+    if (!muntGepakt[i]) {
+      image(muntImg, muntX[i], muntY[i], 50, 50);
+    }
   }
 
   image(karakter, spelerX, spelerY, 75, 75);
 
   textSize(30);
-  fill('white');
-  text("score: " + score, 20, 40); 
-  highScore == 0 ? console.log("") : text("score: " + highScore, 20, 40); 
+  fill("white");
+  text("Jonko vrij: " + score, 20, 40);
+  text("Highscore: " + highScore, 20, 80);
+  text("Munten gepakt: " + muntenGepakt, 20, 120);
 };
 
+// Reset het spel
 var resetGame = function() {
   spelerX = 430;
-  spelerY = 200; 
+  spelerY = 200;
   vijandX = [800, 1200, 1600, 2000, 2400];
   vijandY = [600, 400, 200, 100, 300];
-  spelStatus = SPELEN; 
+  muntX = [1000, 1400, 1800, 2200, 2600];
+  muntY = [500, 300, 100, 200, 400];
+  muntGepakt = [false, false, false, false, false];
+  spelStatus = SPELEN;
+  vijandSnelheid = 3;
 };
 
 function preload() {
-  img = loadImage('Illustraties/Achtergrond.png'); 
-  karakter = loadImage('Illustraties/karakter.png');
-  SpelerImg = loadImage('Illustraties/Speler.png');
+  img = loadImage("Illustraties/Achtergrond.png");
+  karakter = loadImage("Illustraties/karakter.png");
+  SpelerImg = loadImage("Illustraties/Speler.png");
+  muntImg = loadImage("Illustraties/munt.png");
 }
 
 function setup() {
-  createCanvas(1280, 720); // maak een canvas van 960 x 540 pixels
-  frameRate(60); // zet de framesnelheid op 60 frames per seconde
+  createCanvas(1280, 720); // Maak een canvas van 1280 x 720 pixels
+  frameRate(60); // Zet de framesnelheid op 60 frames per seconde
 }
 
 function draw() {
@@ -122,12 +173,19 @@ function draw() {
     verwerkBotsing();
     tekenAlles();
   } else if (spelStatus === GAMEOVER) {
+    if (score > highScore) {
+      highScore = score;
+    }
     score = 0;
-    highScore < score ? highScore = score : console.log("Jonkogepakt")
+    muntenGepakt = 0;
     textSize(40);
-    fill('white');
+    fill("white");
     textAlign(CENTER, CENTER);
-    text("Snoop Dogg heeft de jonko, Druk enter om opnieuw te gaan", width / 2, height / 2);
+    text(
+      "Snoop Dogg heeft de jonko, Druk enter om opnieuw te gaan",
+      width / 2,
+      height / 2
+    );
   }
 }
 
